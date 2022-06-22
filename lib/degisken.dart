be5f4f8f85cf44a28be51ecf50da86fce11537e3
Ertuğrul Kayış <ekayis15@posta.pau.edu.tr>
2022-06-22 14:58:31 +0300

    degisken.dart added

---
+++ lib/degisken.dart
@@ -0,0 +1,31 @@
+void main() {
+  var fiyat = 12.99;
+  double ucret = 23.59;
+
+  print(fiyat);
+  print(ucret);
+
+  fiyat = 57.99;
+  ucret = 10.45;
+
+  print('son  fiyat :$fiyat');
+  print('son  ucret :$ucret');
+
+  //ornek2
+
+  var sonuc1;
+  int sonuc2;
+
+  sonuc1 = 333;
+  sonuc2 = 555;
+
+  print(sonuc1);
+  print(sonuc2);
+
+  var s1, s2, s3;
+
+  s1 = 10;
+  s2 = 20;
+  s3 = 30;
+  print("$s1 $s2 $s3");
+}
