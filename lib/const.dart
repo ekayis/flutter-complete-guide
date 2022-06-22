4a16467d87ae6aad8a3066a5b54d4d5ad2e552e0
Ertuğrul Kayış <ekayis15@posta.pau.edu.tr>
2022-06-22 15:36:13 +0300

    const.dart added

---
+++ lib/const.dart
@@ -0,0 +1,6 @@
+void main(List<String> args) {
+  final sonuc = 100; // sabittir değiştirelemez.
+  final pi = 3.14;
+  final String mesaj = "merhaba";
+  const double a = 19.1; //final performansta bi tık daha iyi.
+}
