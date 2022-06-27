a15e88fb40c7e4962ce710a4b963b0237fdce834
Ertuğrul Kayış <ekayis15@posta.pau.edu.tr>
2022-06-27 17:49:07 +0300

    listogrenciexercises

---
+++ lib/list/listdongu.dart
@@ -0,0 +1,17 @@
+void main() {
+  var meyveler = <String>[];
+
+  meyveler.add("çilek");
+  meyveler.add("muz");
+  meyveler.add("elma");
+  meyveler.add("kivi");
+  meyveler.add("kiraz");
+
+  for (var m in meyveler) {
+    print('Sonuç : $m');
+  }
+
+  for (var i = 0; i < meyveler.length; i++) {
+    print("$i. indeksteki veri : ${meyveler[i]}");
+  }
+}
